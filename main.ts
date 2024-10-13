d61d40ab820eb1d813944ebc727b07e9dce8c634
Nasereddine Lakhal <naserlakhal@gmail.com>
2024-10-13 17:21:25 +0100

    adding the route and the controllers inside the main

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 import { Hono } from 'hono'
 
+import auth from './routes/auth/index.ts'
+import dashbaord from './routes/app/index.ts'
+
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route('/auth', auth)
+app.route('/app', dashbaord)
+
 
 Deno.serve(app.fetch)
